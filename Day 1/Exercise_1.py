52407a14af6e47f57f8523888a2c28b3369eff5c
niravcenation <76927258+niravcenation@users.noreply.github.com>
2022-07-09 12:04:41 +0530

    Add files via upload

---
+++ Day 1/Exercise_1.py
@@ -0,0 +1,6 @@
+# Random Band Name Generator
+
+print("\t\t\t------Welcome to the Band Generator name------")
+city_name = input("\tWhat's the name of your city you grew up in?\n\t")
+pet_name = input("\tWhat's your pet's name?\n\t")
+print(f'\tYour band name could be {city_name} {pet_name} ')
---
+++ Day 1/Input_function.py
@@ -0,0 +1,6 @@
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+#Write your code below this line 👇
+
+name = input("What is your name?")
+print(len(name))
