eb9caca50ebe3d4a18e8b3f73446691d5161094f
niravcenation <76927258+niravcenation@users.noreply.github.com>
2022-07-13 13:15:57 +0530

    Update Life_in_weeks_3.py

--- Day 2/Life_in_weeks_3.py
+++ Day 2/Life_in_weeks_3.py
@@ -1,10 +1,6 @@
 # Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
 
-# 🚨 Don't change the code below 👇
 age = input("What is your current age?")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 years_remaining = 90 - int(age)
 month_remaining = years_remaining * 12
