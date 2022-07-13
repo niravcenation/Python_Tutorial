1611a1c20d1a7cdbfcadd0c51839014443406057
niravcenation <76927258+niravcenation@users.noreply.github.com>
2022-07-13 13:14:26 +0530

    Update and rename BMI_Calculator.py to BMI_Calculator_2.py

--- Day 2/BMI_Calculator_2.py
+++ Day 2/BMI_Calculator_2.py
@@ -1,12 +1,8 @@
 # Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
 
-# 🚨 Don't change the code below 👇
+
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
-# 🚨 Don't change the code above 👆
 
-# BMI = Weight / (height*height)
-
-#Write your code below this line 👇
 BMI = float(weight) / (float(height)*float(height))
 print(int(BMI))
